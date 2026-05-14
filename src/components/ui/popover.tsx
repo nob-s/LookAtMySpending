3c8fd2a864119aff8abc147823d3cc2a97762bc4
nob-s <jqlee999@gmail.com>
2026-05-14 13:56:24 +0800

    Revert "Fix reference"
    This reverts commit a10ef5163bc2390806e645cfb76c7c14175c5a8a.

--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { Popover as PopoverPrimitive } from "radix-ui"
-import { cn } from "../../lib/utils.ts";
+
+import { cn } from "@/lib/utils"
 
 function Popover({
   ...props
